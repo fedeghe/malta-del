fd06750861b5e994e3a508968540c1810825c4a7
fedeghe <fedeghe@gmail.com>
2019-07-06 22:57:08 +0200

    reject set

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var path = require('path'),
-	fs = require('fs');
+    fs = require('fs');
 
 function malta_del(o, options) {
-	var self = this,
-		start = new Date(),
-		msg,
-		pluginName = path.basename(path.dirname(__filename));
+    var self = this,
+        start = new Date(),
+        msg,
+        pluginName = path.basename(path.dirname(__filename));
 
-	return function (solve, reject){
-		var dir = path.dirname(o.name);
-		fs.unlink(dir + '/' +  options.name, function (err) {
+    return function (solve, reject) {
+        var dir = path.dirname(o.name);
+        fs.unlink(dir + '/' + options.name, function (err) {
             err && self.doErr(err, o, pluginName);
-			msg = 'plugin ' + pluginName.white() + ' deleted ' + dir + '/' +  options.name;
-            solve(o);
-  			self.notifyAndUnlock(start, msg);
-		});
-	};
+            msg = 'plugin ' + pluginName.white() + ' deleted ' + dir + '/' + options.name;
+            err
+                ? reject(`Plugin ${pluginName} deletion error:\n${err}`)
+                : solve(o);
+            self.notifyAndUnlock(start, msg);
+        });
+    };
 }
 // malta_del.ext = ['*'];
 module.exports = malta_del;
